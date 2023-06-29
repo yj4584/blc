a8a0cc7d2b532504708bbcf8afb921dea246d660
yj4584 <yj.kim@webtoonguide.com>
2023-06-29 16:57:44 +0900

    링크 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,28 +6,63 @@ export function Header() {
   const [showSteelSubMenu, setShowSteelSubMenu] = useState(false);
   return (
     <section className="header-section">
-      <div className="logo"></div>
+      <NavLink to="/">
+        <div className="logo"></div>
+      </NavLink>
       <div className="menus">
         <ul className="menu-list">
-          <li className="menu"><NavLink to="/intro">회사소개</NavLink>
-            </li>
-          <li className="menu" onMouseEnter={()=>{setShowWoodSubMenu(true)}} onMouseLeave={()=>{setShowWoodSubMenu(false)}}>
-            <NavLink to="/wood">합성목재</NavLink>
-          <ul className={`sub-menu ${showWoodSubMenu?'visible':'invisible'}`}>
-            <li className="sub-menu-item"><NavLink to="/wood">데크&클립</NavLink></li>
-            <li className="sub-menu-item"><NavLink to="/fence">휀스&브라켓</NavLink></li>
-          </ul>
+          <li className="menu">
+            <NavLink to="/intro">회사소개</NavLink>
           </li>
-          <li className="menu" onMouseEnter={()=>{setShowSteelSubMenu(true)}} onMouseLeave={()=>{
-            setShowSteelSubMenu(false)
-          }}>
+          <li
+            className="menu"
+            onMouseEnter={() => {
+              setShowWoodSubMenu(true);
+            }}
+            onMouseLeave={() => {
+              setShowWoodSubMenu(false);
+            }}
+          >
+            <NavLink to="/wood">합성목재</NavLink>
+            <ul
+              className={`sub-menu ${
+                showWoodSubMenu ? "visible" : "invisible"
+              }`}
+            >
+              <li className="sub-menu-item">
+                <NavLink to="/wood">데크&클립</NavLink>
+              </li>
+              <li className="sub-menu-item">
+                <NavLink to="/fence">휀스&브라켓</NavLink>
+              </li>
+            </ul>
+          </li>
+          <li
+            className="menu"
+            onMouseEnter={() => {
+              setShowSteelSubMenu(true);
+            }}
+            onMouseLeave={() => {
+              setShowSteelSubMenu(false);
+            }}
+          >
             <NavLink to="/steelwork">연결철물&주춧돌</NavLink>
-            <ul className={`sub-menu ${showSteelSubMenu?'visible':'invisible'}`}>
-            <li className="sub-menu-item"><NavLink to="/steelwork">연결철물</NavLink></li>
-            <li className="sub-menu-item"><NavLink to="/stone">주춧돌</NavLink></li>
-          </ul>
-            </li>
-          <li className="menu"><NavLink to="/facility">공장 및 목재창고</NavLink></li>
+            <ul
+              className={`sub-menu ${
+                showSteelSubMenu ? "visible" : "invisible"
+              }`}
+            >
+              <li className="sub-menu-item">
+                <NavLink to="/steelwork">연결철물</NavLink>
+              </li>
+              <li className="sub-menu-item">
+                <NavLink to="/stone">주춧돌</NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className="menu">
+            <NavLink to="/facility">공장 및 목재창고</NavLink>
+          </li>
           {/* <li className="menu"><NavLink to="/service">고객센터</NavLink></li> */}
         </ul>
       </div>
